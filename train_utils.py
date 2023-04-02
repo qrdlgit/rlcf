0faa67d3b722796804373728900c089d4d8fb2a7
qrdlgit <129564070+qrdlgit@users.noreply.github.com>
2023-04-02 16:42:22 -0700

    Update train_utils.py

--- train_utils.py
+++ train_utils.py
@@ -22,3 +22,24 @@ def update_generated_code_files(generated_code_file, improvements):
 
     with open(generated_code_file, "w") as f:
         f.write(code)
+
+def get_prompt_for_code_improvement(prompt_file, code_file):
+    with open(prompt_file, "r") as f:
+        prompt = f.read()
+
+    with open(code_file, "r") as f:
+        code = f.read()
+
+    improvement_prompt = f"{code}\n{prompt}"
+    return improvement_prompt
+
+
+def generate_code_improvements(prompt_file, code_file, tokenizer, model):
+    improvement_prompt = get_prompt_for_code_improvement(prompt_file, code_file)
+
+    inputs = tokenizer.encode(improvement_prompt, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
+    generated_improvement = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return (code_file, generated_improvement)
+
