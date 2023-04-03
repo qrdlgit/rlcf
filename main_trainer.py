d324e695d35378b68545169557efb939f0ebeb7f
qrdlgit <129564070+qrdlgit@users.noreply.github.com>
2023-04-02 17:42:24 -0700

    Update main_trainer.py

--- main_trainer.py
+++ main_trainer.py
@@ -12,26 +12,24 @@ from training_utils import (
 def main(input_file):
     # Read input data
     prompt_and_code_files = get_prompts_and_code_filenames(input_file)
-
+    code_files = [x[1] for x in prompt_and_code_files]
+    
     # RLCF iterative process
     num_iterations = 5
     for iteration in range(num_iterations):
         print(f"Iteration {iteration + 1}")
 
         # Generate code improvements using the current model
-        generated_code_improvements = generate_code_improvements(prompt_and_code_files)
+        (improvement_prompt, code_file, generated_improvement) = generate_code_improvements(prompt_and_code_files[0], prompt_and_code_files[1], tokenizer, model)
 
         # Update generated code files with improvements
-        update_generated_code_files(generated_code_improvements, prompt_and_code_files)
+        update_generated_code_files(code_file, generated_improvement)
 
         # Evaluate the generated code improvements
-        evaluation_results = evaluate_code_improvements(generated_code_improvements, prompt_and_code_files)
-
-        # Calculate rewards based on evaluation results
-        rewards = calculate_rewards(evaluation_results)
+        rewards = get_rewards_for_code_improvements([code_file])
 
         # Prepare the dataset for training
-        dataset = prepare_dataset(prompt_and_code_files, generated_code_improvements, rewards)
+        dataset = prepare_dataset([improvement_prompt], [generated_code_improvement], rewards)
 
         # Train the model
         train_model(dataset)
