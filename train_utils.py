c98ffef43864ce97de5932543090d2241870366f
qrdlgit <129564070+qrdlgit@users.noreply.github.com>
2023-04-02 17:42:20 -0700

    Update train_utils.py

--- train_utils.py
+++ train_utils.py
@@ -1,7 +1,19 @@
 
 import json
+from transformers import (
+    GPTNeoForCausalLM,
+    GPT2Tokenizer,
+    TextDataset,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    TrainingArguments,
+)
+
+from torch.utils.data import DataLoader
+from transformers import AdamW, get_linear_schedule_with_warmup
 
 # Update the update_generated_code_files function
+
 def update_generated_code_files(generated_code_file, improvements):
     start_token = "<embed>"
     end_token = "</embed>"
@@ -41,5 +53,66 @@ def generate_code_improvements(prompt_file, code_file, tokenizer, model):
     outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
     generated_improvement = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    return (code_file, generated_improvement)
+    return (improvement_prompt, code_file, generated_improvement)
 
+
+from datasets import Dataset
+
+class CodeImprovementDataset(Dataset):
+    def __init__(self, prompts, responses, rewards):
+        self.data = []
+        for i in range(len(prompts)):
+            prompt = prompts[i]
+            response = responses[i]
+            reward = rewards[i]
+
+            self.data.append({
+                "prompt": prompt,
+                "response": response,
+                "reward": reward
+            })
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
+
+def prepare_dataset(prompts, responses, rewards):
+    return CodeImprovementDataset(prompts, responses, rewards)
+
+
+
+def train_model(model, dataset, epochs=1, batch_size=8, learning_rate=5e-5, weight_decay=0.01, warmup_steps=0):
+    # Create a DataLoader for the dataset
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    # Setup the optimizer and learning rate scheduler
+    optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=len(data_loader) * epochs)
+
+    # Fine-tune the model
+    model.train()
+    for epoch in range(epochs):
+        for batch in data_loader:
+            prompts = batch["prompt"]
+            responses = batch["response"]
+            rewards = batch["reward"]
+
+            # Encode the input (prompts) and target (responses) texts
+            input_encoding = tokenizer(prompts, return_tensors="pt", padding=True, truncation=True)
+            target_encoding = tokenizer(responses, return_tensors="pt", padding=True, truncation=True)
+
+            # Calculate the model's loss based on the rewards
+            outputs = model(**input_encoding, labels=target_encoding.input_ids)
+            loss = outputs.loss
+            weighted_loss = loss / rewards
+
+            # Backward pass and optimization step
+            optimizer.zero_grad()
+            weighted_loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+        # Save the model after each epoch
+        model.save_pretrained(f"codebert_finetuned_epoch_{epoch + 1}")
